4a832cc887a127240c892ba13877689c0679ca32
SaraToth <saratoth101@gmail.com>
2024-09-05 16:36:03 +0900

    Create event listeners for the previous and next buttons, create the logic to track which photo is shown using photosIndex, and logic to update the Index value if the user scrolls past the first or last photo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+//The first photo, by default is index 1
+let photosIndex = 1;
+
+const photos = document.querySelectorAll(".carousel-photo");
+const previous = document.querySelector(".btn-previous");
+const next = document.querySelector(".btn-next");
+
+//Updates index when the user scrolls pass the first, or last photo respectively in the carousel
+if(photosIndex < 1) {
+    photosIndex = photos.length;
+} else if(photosIndex > photos.length) {
+    photosIndex = 1;
+}
+
+previous.addEventListener("click", () => {
+    console.log("previous");
+})
+
+next.addEventListener("click", () => {
+    console.log("next");
+})
+
+
+
+
+
+
+
+
+
+
+
+
