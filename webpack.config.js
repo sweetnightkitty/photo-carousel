774ea53d204dc0b0d17709164fa1f29d0dbc7ee0
SaraToth <saratoth101@gmail.com>
2024-09-05 15:40:15 +0900

    Add html-loader dev dependency to webpack for the local project files

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      
     ],
   },
 };
